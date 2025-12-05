0bc51ae88277d8b809376361ae587724798ddd51
MrNopi <Smart.like1998@gmail.com>
2025-12-05 12:07:16 +0200

    Update Program.cs

--- MyNewTestProject/Program.cs
+++ MyNewTestProject/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+String[] arr = new String[];
+
+arr[0] = "Hello";
+arr[1] = "World";
+arr[2] = "!!";
+
 app.MapGet("/", () => "Hello World!!");
+app.MapGet("/hello" => "Hello World!");
+app.MapGet("/hi" => arr);
 
 app.Run();
